99de559c2802dfe7efd62050abf290055572b467
Rajesh <techyraj2@gmail.com>
2024-06-27 15:24:31 +0530

    Intial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        PurchaseNow_v2
+      </header>
+    </div>
+  );
+}
+export default App;
+  
